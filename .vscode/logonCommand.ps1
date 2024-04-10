2b644fd6a2c2fba4c431a37a642b59b4b867a89a
bohnanza22 <bohnanza22@gmail.com>
2024-04-10 13:21:14 -1000

    LogonCommand

--- .vscode/logonCommand.ps1
+++ .vscode/logonCommand.ps1
@@ -1,17 +1,17 @@
-Write-host "Testing has started..." -ForegroundColor Cyan
-Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\master\Deploy-Application.exe" -Wait
-Write-host "Installation completed" -ForegroundColor DarkGreen
-Write-host "you have 60 seconds to verify the installation before it is automatically uninstalled" -ForegroundColor Cyan
+Write-Host 'Testing has started...' -ForegroundColor Cyan
+Start-Process -FilePath "C:\Users\WDAGUtilityAccount\$Application\main\Deploy-Application.exe" -Wait
+Write-Host 'Installation completed' -ForegroundColor DarkGreen
+Write-Host 'you have 60 seconds to verify the installation before it is automatically uninstalled' -ForegroundColor Cyan
 
 $Seconds = 60
 $EndTime = [datetime]::UtcNow.AddSeconds($Seconds)
 
 while (($TimeRemaining = ($EndTime - [datetime]::UtcNow)) -gt 0) {
-  Write-Progress -Activity 'Waiting for...' -Status testing -SecondsRemaining $TimeRemaining.TotalSeconds
-  Start-Sleep 1
+    Write-Progress -Activity 'Waiting for...' -Status testing -SecondsRemaining $TimeRemaining.TotalSeconds
+    Start-Sleep 1
 }
 
-Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\master\Deploy-Application.exe" -ArgumentList "Uninstall" -Wait
-Write-host "test completed" -ForegroundColor DarkGreen
-Write-host "You can close sandbox now!" -ForegroundColor Cyan
-Read-Host -Prompt "Press any key to continue..."
+Start-Process -FilePath "C:\Users\WDAGUtilityAccount\$Application\main\Deploy-Application.exe" -ArgumentList 'Uninstall' -Wait
+Write-Host 'test completed' -ForegroundColor DarkGreen
+Write-Host 'You can close sandbox now!' -ForegroundColor Cyan
+Read-Host -Prompt 'Press any key to continue...'
