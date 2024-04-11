366e62e58bafbca8c75fe7b34576b39fcf8f7caf
bohnanza22 <bohnanza22@gmail.com>
2024-04-10 15:17:55 -1000

    Added parameter to pass the $Application variable from Sandbox.ps1 to the LogonCommand.ps1 to dynamically determine the folder name on the desktop

--- .vscode/logonCommand.ps1
+++ .vscode/logonCommand.ps1
@@ -1,17 +1,40 @@
-Write-Host 'Testing has started...' -ForegroundColor Cyan
-Start-Process -FilePath "C:\Users\WDAGUtilityAccount\$Application\main\Deploy-Application.exe" -Wait
-Write-Host 'Installation completed' -ForegroundColor DarkGreen
-Write-Host 'you have 60 seconds to verify the installation before it is automatically uninstalled' -ForegroundColor Cyan
+#LogonCommand.ps1
 
-$Seconds = 60
-$EndTime = [datetime]::UtcNow.AddSeconds($Seconds)
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [String]$Application,
+    [int]$VerificationTime = 120  # Optional parameter for verification time
+)
 
-while (($TimeRemaining = ($EndTime - [datetime]::UtcNow)) -gt 0) {
-    Write-Progress -Activity 'Waiting for...' -Status testing -SecondsRemaining $TimeRemaining.TotalSeconds
-    Start-Sleep 1
+$deployPath = "C:\Users\WDAGUtilityAccount\Desktop\$Application"
+
+if (Test-Path -Path "$deployPath\Deploy-Application.exe") {
+    Write-Host 'Testing has started...' -ForegroundColor Cyan
+
+    Start-Process -FilePath "$deployPath\Deploy-Application.exe" -Wait
+    Write-Host 'Installation completed' -ForegroundColor DarkGreen
+
+    Write-Host "You have $VerificationTime seconds to verify the installation before it is automatically uninstalled." -ForegroundColor Cyan
+
+    $EndTime = [datetime]::UtcNow.AddSeconds($VerificationTime)
+
+    while (($TimeRemaining = ($EndTime - [datetime]::UtcNow)) -gt 0) {
+        Write-Progress -Activity 'Waiting for...' -Status testing -SecondsRemaining $TimeRemaining.TotalSeconds
+        Start-Sleep 1
+    }
+
+    if (!(Read-Host -Confirm "Are you sure you want to uninstall $Application?")) {
+        Write-Host 'Uninstall cancelled.' -ForegroundColor Yellow
+        Exit
+    }
+
+    Start-Process -FilePath "$deployPath\Deploy-Application.exe" -ArgumentList 'Uninstall' -Wait
+    Write-Host 'test completed' -ForegroundColor DarkGreen
+} else {
+    Write-Error "Deploy-Application.exe not found in $deployPath"
 }
 
-Start-Process -FilePath "C:\Users\WDAGUtilityAccount\$Application\main\Deploy-Application.exe" -ArgumentList 'Uninstall' -Wait
-Write-Host 'test completed' -ForegroundColor DarkGreen
 Write-Host 'You can close sandbox now!' -ForegroundColor Cyan
 Read-Host -Prompt 'Press any key to continue...'
+
